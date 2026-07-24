02f1f65feeae05db5ee54f1a07a7c4e4142a5146
Maxdev17 <midowu1702@gmail.com>
2026-07-24 23:43:56 +0100

    updated javascript so that carousel effect for home page does not get tangled up with the other pages

--- script.js
+++ script.js
@@ -2,34 +2,36 @@ const track = document.querySelector(".carousel-track");
 const dots = document.querySelectorAll(".dot");
 let currentIndex = 0;
 
-function goToSlide(index) {
-  track.style.transform = `translateX(-${index * 100}%)`;
-  dots.forEach((dot) => dot.classList.remove("active"));
-  dots[index].classList.add("active");
-  currentIndex = index;
-}
-
-dots.forEach((dot) => {
-  dot.addEventListener("click", () => {
-    track.style.transition = "transform 0.5s ease";
-    const index = parseInt(dot.dataset.index);
-    goToSlide(index);
-  });
-});
-
-setInterval(() => {
-  const nextIndex = currentIndex + 1;
-
-  if (nextIndex >= dots.length) {
-    track.style.transition = "none";
-    goToSlide(0);
-    track.offsetHeight;
-    track.style.transition = "transform 0.5s ease";
-  } else {
-    track.style.transition = "transform 0.5s ease";
-    goToSlide(nextIndex);
+if (track) {
+  function goToSlide(index) {
+    track.style.transform = `translateX(-${index * 100}%)`;
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+    currentIndex = index;
   }
-}, 3000);
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", () => {
+      track.style.transition = "transform 0.5s ease";
+      const index = parseInt(dot.dataset.index);
+      goToSlide(index);
+    });
+  });
+
+  setInterval(() => {
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex >= dots.length) {
+      track.style.transition = "none";
+      goToSlide(0);
+      track.offsetHeight;
+      track.style.transition = "transform 0.5s ease";
+    } else {
+      track.style.transition = "transform 0.5s ease";
+      goToSlide(nextIndex);
+    }
+  }, 3000);
+}
 
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
